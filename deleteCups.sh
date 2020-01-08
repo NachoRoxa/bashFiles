d4ab03810f6db55b689466efc01669340cf79c32
NachoRoxa <ig.rocha.pavez@gmail.com>
2020-01-08 18:15:21 -0300

    change deleteCups

--- deleteCups.sh
+++ deleteCups.sh
@@ -1,2 +1,21 @@
 rm -rf /usr/var/spool/cups
 service cups restart
+
+#Solo en caso de que no funcione el service descomentar
+#las lineas de abajo
+
+#cancel -a gk1
+#cancel -a gk2
+#cancel -a gk3
+#cancel -a gk4
+#cancel -a gk5
+#cancel -a gk6
+#cancel -a gk7
+#cancel -a gk8
+#cancel -a gk9
+#cancel -a gk10
+#cancel -a gk11
+#cancel -a gk12
+#cancel -a gk13
+#cancel -a gk14
+#cancel -a gk15
